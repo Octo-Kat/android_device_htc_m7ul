d29c13e2b9cb24ae17e9a18c7488b6571ab0f3ef
Carl Windhausen <cjwindhausen@gmail.com>
2013-12-30 13:59:34 -0500

    Update and rename cm.mk to oct.mk

--- oct.mk
+++ oct.mk
@@ -6,5 +6,5 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=KRT16S.H5 BUILD_FINGERPRINT="htc/m7_google/m7:4.4/KRT16S.H5/280886:user/release-keys" PRIVATE_BUILD_DESC="3.58.1700.5 CL280886 release-keys"
 
-PRODUCT_NAME := cm_m7ul
+PRODUCT_NAME := oct_m7ul
 PRODUCT_DEVICE := m7ul
